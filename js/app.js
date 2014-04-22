2ac365e27d8ae9b3555631416f09423c1e033c36
G <giovannivajnadepava@gmail.com>
2014-04-22 22:14:36 +0200

    Second round.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 /**
  * Created by G on 4/8/2014.
  */
+
+var app=angular.module('app', []);
+
+app.controller('ctrl', function($scope) {
+  $scope.scroll=function scroll() {
+    $scope.legend='ticker-scroller-left';
+  };
+
+  $scope.next=function next() {
+    $scope.legend='disappear-left';
+  };
+});
